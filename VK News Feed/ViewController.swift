4df6d5f8626a5ef06f0f846e1bcb87da8be17fb6
Roman <rrakhlin@gmail.com>
2020-02-09 15:33:15 +0300

    Initial Commit

---
+++ VK News Feed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK News Feed
+//
+//  Created by Roman Rakhlin on 09.02.2020.
+//  Copyright © 2020 Roman Rakhlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
